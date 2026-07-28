70006c578d951dba97e80329aeeab8150367a5b2
FUJII-573 <adkzom123@gmail.com>
2026-07-28 22:54:28 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  base: '/WEB/',
-})
+  plugins: [react(), tailwindcss()],
+  root: './client', // 👈 ชี้ root ไปที่ client
+  build: {
+    outDir: '../dist', // 👈 ให้ build ออกไปที่โฟลเดอร์ dist ด้านนอก
+    emptyOutDir: true,
+  },
+});
